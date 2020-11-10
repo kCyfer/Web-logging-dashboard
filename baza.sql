0ad5bd133135890322152eea0ca6f5ac3a7bf8e3
kCyfer <notimportantgoing@gmail.com>
2020-11-10 21:59:37 +0100

    Change database options.

--- baza.sql
+++ baza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 10 Lis 2020, 20:11
+-- Czas generowania: 10 Lis 2020, 21:58
 -- Wersja serwera: 10.4.14-MariaDB
 -- Wersja PHP: 7.4.11
 
@@ -28,10 +28,39 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `id` int(3) NOT NULL,
-  `username` varchar(15) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
+(1, 'administrator', '$2y$10$qtkFxfrhA2X.wVzHMO63b.3SVhN/pa6hzqrqNKNcLGieRrRxmvt..', '2020-11-10 21:57:59');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
